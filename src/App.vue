88086a1ae081645abf20a65b1f3ec48029ac93e8
Chun Zheng <chun.zheng@bronto.com>
2018-10-25 21:15:29 -0400

    meh

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,34 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-toolbar dark color="primary">
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title class="white--text">Chun Zheng</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-toolbar-items>
+                    <v-btn flat>Comics</v-btn>
+                    <v-btn flat>PillowFight</v-btn>
+                    <v-btn flat>Frowning</v-btn>
+                    <v-btn flat>Contact</v-btn>
+                </v-toolbar-items>
+        </v-toolbar>
+        <v-content>
+            <HelloWorld/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data () {
+        return {
+            //
+        }
     }
-  }
 }
 </script>
---
+++ src/components/Frowning.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-carousel
+        :hide-controls=true
+        :multiple=true
+        :light=true
+        :hide-delimiters=true
+        >
+        <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            ></v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            items: 
+            [
+                {
+                    src: require('../assets/cat.jpg')
+                },
+                {
+                    src: require('../assets/cat1.jpg')
+                },
+                {
+                    src: require('../assets/cat2.jpg')
+                },
+                {
+                    src: require('../assets/cat3.jpg')
+                },
+                {
+                    src: require('../assets/cat4.jpg')
+                },
+                {
+                    src: require('../assets/cat5.jpg')
+                },
+
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
